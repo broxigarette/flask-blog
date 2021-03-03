7b06859fa7a862ffef1546e998119bfe898b109c
Broxigar <edeltitnu@gmail.com>
2021-03-03 22:25:46 +0300

    Initialization project

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SECRET_KEY = '91d62bdb6b9dde5b5ed369fdd1c745db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
